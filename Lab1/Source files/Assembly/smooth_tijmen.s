f82f1feb4ee85ee2425b43c3e35a2991ebfd3542
Tijmen Verhulsdonck <tijmenlv@me.com>
2016-05-24 14:10:30 +0200

    Update smooth_tijmen.s

--- Lab1/Source files/Assembly/smooth_tijmen.s
+++ Lab1/Source files/Assembly/smooth_tijmen.s
@@ -1,9 +1,9 @@
 .data
 NCOEFFS: .word 3
-coeff:  .double 1.5, -1.0,  0.5
-NSAMPLES: .word 25
-sample: .double 1.7,  2.6, -1.3,  4.2, -6.1, -7.6,  23.0, -0.1, 43.0, -4.5,  6.9, -16.7, 8.9, -6.28, 9.8, -23.6, 13.7, 4.3, 0.56, -0.78, -9.2, 7.67, -13.13, 18.24, 19.0
-result: .double 0.0,  0.0,  0.0,  0.0,  0.0,  0.0,   0.0,  0.0,  0.0,  0.0,  0.0,   0.0, 0.0,   0.0, 0.0,   0.0,  0.0, 0.0,  0.0,   0.0,   0.0, 0.0,    0.0,   0.0,  0.0
+coeff:  .double 0.5, 1.0,  0.5
+NSAMPLES: .word 5
+sample: .double 1,2,1,2,1
+result: .double 0,0,0,0,0
 ze: .double 0.0
 onei: .word 1
 onef: .double 1
@@ -22,13 +22,12 @@ Mask: .word 0x7fffffffffffffff
 	#Calc norm val for 3 coeffs and setup first and last result
 		l.d 	f5,coeff(r10)	
 		l.d 	f1,sample(r0)
-		c.lt.d 	f5,f0
-		daddi 	r14,r10,8
+		daddi 	r10,r10,8
 		mfc1 	r7,f5       			# load coeff as integer
 		and  	r7,r7,r4				# mask sign bit
 		lw	r1,onei(r0) 				# setup int reg with value 1
-		mtc1 	r7,f2					# store the coeff as positive float 
-		add.d 	f4,f4,f2 				# add absolute value to norm
+		mtc1 	r7,f5					# store the coeff as positive float 
+		add.d 	f4,f4,f5 				# add absolute value to norm
 
 				
 		l.d 	f6,coeff(r10)        	# load the next coeff value
@@ -38,35 +37,34 @@ Mask: .word 0x7fffffffffffffff
 		mfc1 	r7,f6       			# load coeff as integer
 		l.d 	f11,onef(r0) 				#used to load zero value = 0.0				
 		and  	r7,r7,r4				# mask sign bit
-		mtc1 	r7,f2					# store the coeff as positive float 
-		add.d 	f4,f4,f2 				# add absolute value to norm
+		mtc1 	r7,f6					# store the coeff as positive float 
+		add.d 	f4,f4,f6 				# add absolute value to norm
 
 		l.d 	f7,coeff(r10)        	# load the next coeff value
 		s.d	f8,result(r2)			#store the n-1 value in result
 		daddi	r11,r1,7
 		mfc1 	r7,f7       			# load coeff as integer
 		and  	r7,r7,r4				# mask sign bit
-		mtc1 	r7,f2					# store the coeff as positive float 
-		add.d 	f4,f4,f2 				# add absolute value to norm
+		mtc1 	r7,f7					# store the coeff as positive float 
+		add.d 	f4,f4,f7 				# add absolute value to norm
 		div.d 	f12,f11,f4				#inverse norm
 
-		#coeff  0 = f5, 2 = f6, 3 = f7
+		#coeff  0 = f5, 1 = f6, 2 = f7
 	MasterLoop:
 			l.d 	f22,sample(r6)			# load sample
-
 			mul.d 	f20,f22,f5			# calculate result value
-			mov.d 	f24,f0   				# norm = 0.0	
-			l.d 	f22,sample(r11)		# load sample 
+			mov.d 	f24,f0   			# norm = 0.0	
 
-			mul.d 	f21,f22,f6			# calculate result value
-			daddi 	r11,r11,8	
-			add.d 	f24,f24,f20			#accumulate result partial vlaues
 			l.d 	f22,sample(r11)		# load sample 
-			add.d 	f24,f24,f21			#accumulate result partial values
+			daddi 	r11,r11,8	
+			mul.d 	f21,f22,f6			# calculate result value
+
+			l.d 	f22,sample(r11)		# load sample 
+			add.d 	f24,f24,f20			#accumulate result partial vlaues
 			mul.d 	f23,f22,f7			# calculate result value
 
+			add.d 	f24,f24,f21			#accumulate result partial values
 			add.d 	f24,f24,f23			#accumulate result partial values
-
 			mul.d 	f24,f24,f12			# normalize
 			daddi 	r6,r11,-8
 		bne 	r11,r2, MasterLoop	 	# if i = NSAMPLES then exit Normloop
